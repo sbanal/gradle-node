6a50f64e47704fe9bbe96677d3f0c4d3587379b3
sbanal <stephen.banal@gmail.com>
2018-05-28 23:19:13 +1000

    karma chrome headlesss fix

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,29 @@
 FROM gradle:4.7.0-jdk8-alpine
 
 USER root
+RUN apk update
 RUN apk add --update nodejs 
+RUN apk -U --no-cache \
+	--allow-untrusted add \
+    zlib-dev \
+    chromium \
+    xvfb \
+    wait4ports \
+    xorg-server \
+    dbus \
+    ttf-freefont \
+    mesa-dri-swrast \
+    grep \ 
+    udev \
+    && apk del --purge --force linux-headers binutils-gold gnupg zlib-dev libc-utils \
+    && rm -rf /var/lib/apt/lists/* \
+    /var/cache/apk/* \
+    /usr/share/man \
+    /tmp/* \
+    /usr/lib/node_modules/npm/man \
+    /usr/lib/node_modules/npm/doc \
+    /usr/lib/node_modules/npm/html \
+    /usr/lib/node_modules/npm/scripts
 RUN mkdir /workspace
 RUN which node 
 RUN node -v
@@ -9,6 +31,11 @@ RUN npm -v
 RUN which java
 RUN echo "JAVA_HOME:$JAVA_HOME"
 RUN echo "PATH:$PATH"
+RUN ls -l /usr
+#RUN chown -R gradle:gradle /usr/src/app
+
+ENV CHROME_BIN=/usr/bin/chromium-browser
+ENV CHROME_PATH=/usr/lib/chromium/
 
 USER gradle
 WORKDIR /workspace
--- test-app/karma.conf.js
+++ test-app/karma.conf.js
@@ -1,6 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const process = require('process');
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -23,11 +26,39 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: ['progress', 'kjhtml'],
+    //listenAddress: process.env.LOCAL_IP,
+    hostname: process.env.LOCAL_IP,
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    logLevel: config.LOG_DEBUG,
+    autoWatch: false,
+    /*browsers: ['HeadlessChrome'],
+    customLaunchers:{
+	HeadlessChrome:{
+	  base: 'ChromeHeadless',
+	  flags: [
+		 '--no-sandbox',
+		 '--disable-setuid-sandbox',
+		 '--disable-gpu',
+		 '--remote-debugging-port=9222',
+	  ]
+	}
+    },*/
+    browsers: ['ChromeHeadless'],
+    /*browsers: ['Chrome_without_security'],
+    customLaunchers: {
+	    Chrome_without_security: {
+		base: 'ChromeHeadless',
+		flags: [
+			'--no-sandbox',
+			'--disable-setuid-sandbox',
+			'--disable-gpu',
+			'--remote-debugging-port=9222',
+			],
+		debug: true
+		},
+     }, */
+     //concurrency: Infinity,	  
+     singleRun: false
   });
 };
